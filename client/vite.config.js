12ff90f831d6aae94c17b3349354ec1336a2ea68
Ashutosh verma <vkrypto@gmail.com>
2026-05-08 01:37:33 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 const apiTarget = process.env.VITE_API_PROXY_TARGET || 'http://localhost:5001'
 
 export default defineConfig({
+  base: process.env.VITE_BASE || '/',
   plugins: [react()],
   server: {
     proxy: {
